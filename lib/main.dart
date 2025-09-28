7f0edd85e5bf791f1514cc91f445067c83ed1c2b
omzkiii <geogeomarsantos@gmail.com>
2025-09-29 01:00:25 +0800

    test state

--- lib/main.dart
+++ lib/main.dart
@@ -4,21 +4,50 @@ void main() {
   runApp(const App());
 }
 
-class App extends StatefulWidget {
+class App extends StatelessWidget {
   const App({super.key});
 
   @override
-  State<App> createState() => _AppState();
-}
-
-class _AppState extends State<App> {
-  List<String> items = ["foo", "bar", "baz"];
-  @override
   Widget build(BuildContext context) {
+    var lister = ListNotifier();
     return MaterialApp(
       home: Scaffold(
-        body: ListView(children: items.map((item) => Text(item)).toList()),
+        body: Column(
+          children: [
+            Expanded(
+              child: ListenableBuilder(
+                listenable: lister,
+                builder: (context, child) {
+                  return ListView(
+                    children: lister.numbers
+                        .map((item) => Text("$item"))
+                        .toList(),
+                  );
+                },
+              ),
+            ),
+            Text("${lister.numbers}"),
+            TextButton(
+              onPressed: () {
+                lister.addToList();
+              },
+              child: Text("Add"),
+            ),
+          ],
+        ),
       ),
     );
+  }
+}
+
+class ListNotifier extends ChangeNotifier {
+  int _number = 0;
+  final List<int> _numbers = [];
+  List<int> get numbers => _numbers;
+
+  void addToList() {
+    _numbers.add(_number++);
+    print(numbers);
+    notifyListeners();
   }
 }
