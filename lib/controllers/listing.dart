f67e4a217f90e93859b81f9ed89f542e0f814797
omzkiii <geogeomarsantos@gmail.com>
2025-10-03 02:48:29 +0800

    add controllers folder

---
+++ lib/controllers/listing.dart
@@ -0,0 +1,12 @@
+import 'package:app/db.dart';
+import 'package:app/models/listing.dart';
+import 'package:sqflite/sqflite.dart';
+
+void insertListing(Listing listing) async {
+  Database db = await DB.init();
+  db.insert(
+    "listing",
+    listing.toMap(),
+    conflictAlgorithm: ConflictAlgorithm.replace,
+  );
+}
