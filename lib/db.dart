916172a4d18b747e37a24f3d30166da9e4c1e59b
omzkiii <geogeomarsantos@gmail.com>
2025-10-03 02:49:03 +0800

    remove the listing model from db.dart

--- lib/db.dart
+++ lib/db.dart
@@ -4,37 +4,6 @@ import 'package:flutter/widgets.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
-class Listing {
-  final int id;
-  final String name;
-  final int price;
-  final String status;
-  final String desc;
-
-  Listing({
-    required this.id,
-    required this.name,
-    required this.price,
-    required this.status,
-    required this.desc,
-  });
-
-  Map<String, Object?> toMap() {
-    return {
-      'id': id,
-      'name': name,
-      'age': price,
-      'status': status,
-      'desc': desc,
-    };
-  }
-
-  @override
-  String toString() {
-    return 'Listing{id: $id, name: $name, age: $price, status: $status, desc: $desc}';
-  }
-}
-
 class DB {
   static late Future<Database> database;
   static Future<Database> init() async {
