16cf1d91d264d6ff52bfc76679c059f5ee7e3ec9
omzkiii <geogeomarsantos@gmail.com>
2025-10-04 22:21:11 +0800

    add add_listing widget

---
+++ lib/views/add_listing.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class AddListing extends StatelessWidget {
+  const AddListing({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: const Text('Go back!'),
+        ),
+      ),
+    );
+  }
+}
