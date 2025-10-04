ed1188a9ba0303b564f27de33a7b5fe0c6f2be21
omzkiii <geogeomarsantos@gmail.com>
2025-10-04 23:44:22 +0800

    add inputField widget

--- lib/views/add_listing.dart
+++ lib/views/add_listing.dart
@@ -1,19 +1,64 @@
 import 'package:flutter/material.dart';
 
-class AddListing extends StatelessWidget {
+class AddListing extends StatefulWidget {
   const AddListing({super.key});
 
   @override
+  State<AddListing> createState() => _AddListingState();
+}
+
+class _AddListingState extends State<AddListing> {
+  final _formKey = GlobalKey<FormState>();
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: const Text('Go back!'),
+      appBar: AppBar(title: Text("Add Listing")),
+      body: Form(
+        key: _formKey,
+        child: Column(
+          children: [
+            InputField(inputName: "Name"),
+            InputField(inputName: "Description"),
+          ],
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          if (_formKey.currentState!.validate()) {
+            // Navigator.pop(context);
+          }
+        },
+      ),
+    );
+  }
+}
+
+class InputField extends StatelessWidget {
+  final String inputName;
+  const InputField({super.key, required this.inputName});
+
+  @override
+  Widget build(BuildContext context) {
+    return FormField(
+      builder: (context) {
+        return Container(
+          padding: EdgeInsets.all(50),
+          child: Column(
+            children: [
+              TextFormField(
+                decoration: InputDecoration(labelText: inputName),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please input ${inputName.toLowerCase()}.';
+                  }
+                  return null;
+                },
+              ),
+            ],
+          ),
+        );
+      },
     );
   }
 }
