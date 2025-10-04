665f36d3edad14bcac53ca7b33d88470e726a644
omzkiii <geogeomarsantos@gmail.com>
2025-10-04 22:22:00 +0800

    add navigation to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:app/models/listing.dart';
+import 'package:app/views/add_listing.dart';
 import 'package:app/views/listings.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(const App());
+  runApp(const MaterialApp(home: App()));
 }
 
 class App extends StatelessWidget {
@@ -13,39 +14,35 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var listenable = ListingNotifier();
-    return MaterialApp(
-      home: Scaffold(
-        body: Listings(),
+    return Scaffold(
+      body: Listings(),
 
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-        floatingActionButton: FloatingActionButton(
-          onPressed: () async {
-            Listing listing = Listing(
-              name: "foo",
-              price: 99.0,
-              desc: "listed item",
-            );
-            listenable.addToList(listing);
-          },
-          child: Icon(Icons.add),
-        ),
-        bottomNavigationBar: BottomAppBar(
-          elevation: 0.0,
-          child: Row(
-            children: [
-              IconButton(
-                tooltip: 'Open navigation menu',
-                icon: const Icon(Icons.menu),
-                onPressed: () {},
-              ),
-              Spacer(),
-              IconButton(
-                tooltip: 'Search',
-                icon: const Icon(Icons.search),
-                onPressed: () {},
-              ),
-            ],
-          ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => AddListing()),
+          );
+        },
+        child: Icon(Icons.add),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        elevation: 0.0,
+        child: Row(
+          children: [
+            IconButton(
+              tooltip: 'Open navigation menu',
+              icon: const Icon(Icons.menu),
+              onPressed: () {},
+            ),
+            Spacer(),
+            IconButton(
+              tooltip: 'Search',
+              icon: const Icon(Icons.search),
+              onPressed: () {},
+            ),
+          ],
         ),
       ),
     );
