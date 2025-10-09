d55ee9c234525366f8d84d2f7b28c186c7b0c0bb
omzkiii <geogeomarsantos@gmail.com>
2025-10-09 23:59:47 +0800

    add expense card

--- lib/views/expenses.dart
+++ lib/views/expenses.dart
@@ -14,14 +14,29 @@ class Expenses extends StatelessWidget {
       builder: (context, child) {
         listenable.getExpenses(listingId: listingId);
         print(listenable.list);
-        return Expanded(
-          child: ListView(
-            children: listenable.list
-                .map((item) => Text(" ${item.name}"))
-                .toList(),
+        return Container(
+          child: Expanded(
+            child: ListView(
+              children: listenable.list
+                  .map((item) => ExpenseCard(expense: item))
+                  .toList(),
+            ),
           ),
         );
       },
+    );
+  }
+}
+
+class ExpenseCard extends StatelessWidget {
+  final Expense expense;
+  const ExpenseCard({super.key, required this.expense});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [Text(expense.name), Text("PHP ${expense.amount}")],
+      ),
     );
   }
 }
