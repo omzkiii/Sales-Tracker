3a45e8fb560e1ec21c4dacd267292794c19cc190
omzkiii <geogeomarsantos@gmail.com>
2025-10-09 22:54:39 +0800

    revert listing card

--- lib/views/listings.dart
+++ lib/views/listings.dart
@@ -1,7 +1,6 @@
 import 'package:app/controllers/listing.dart';
 import 'package:app/models/listing.dart';
 import 'package:app/views/item.dart';
-import 'package:app/views/listing_card.dart';
 import 'package:flutter/material.dart';
 
 class Listings extends StatelessWidget {
@@ -27,6 +26,29 @@ class Listings extends StatelessWidget {
             },
           ),
         ],
+      ),
+    );
+  }
+}
+
+class ListingCard extends StatelessWidget {
+  final Listing listing;
+  const ListingCard({super.key, required this.listing});
+
+  @override
+  Widget build(BuildContext context) {
+    return Hero(
+      tag: "${listing.id}",
+      child: ListTile(
+        title: Text(listing.name),
+        subtitle: Text(listing.price.toString()),
+        tileColor: Colors.red,
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => Item(item: listing)),
+          );
+        },
       ),
     );
   }
